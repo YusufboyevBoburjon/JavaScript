852eb2239557ed036a11c289c22afa6375bb1bf8
Boburjon <boburjonfusufboyev@gmail.com>
2026-07-25 16:24:51 +0500

    add

---
+++ main.js
@@ -0,0 +1,89 @@
+
+let fruits = ['apple', 'banana'];
+fruits.push("orange", "mango", "peach");
+console.log(fruits);
+
+let colors = ['red', 'green', 'blue', 'yellow'];
+colors.pop();
+console.log(colors);
+
+let numbers = [5, 6, 7];
+numbers.unshift(4, 2);
+console.log(numbers);
+
+let animals = ['cat', 'dog', 'bird', 'fish'];
+animals.shift();
+console.log(animals);
+
+let letters = ['a', 'b', 'c', 'd', 'e'];
+letters.splice(2, 1);
+console.log(letters);
+
+let scores = [10, 20, 30];
+scores.push(22);
+scores.pop();
+console.log(scores);
+
+let names = ['Ali', 'Vali'];
+names.unshift('Bobur');
+names.shift();
+console.log(names);
+
+let nums = [100, 200, 300, 400, 500];
+nums.splice(1, 2);
+console.log(nums);
+
+let emptyArray = [];
+emptyArray.push(200, 400, 600, 800);
+console.log(emptyArray);
+
+let cars = ['BMW', 'Audi', 'Toyota'];
+cars.pop
+console.log(cars);
+let saqlandi = cars.pop()
+console.log(saqlandi);
+
+let ages = [25, 30];
+ages.unshift(10, 15, 20);
+console.log(ages);
+
+let countries = ['Uzbekistan', 'USA', 'UK'];
+countries.shift()
+console.log(countries);
+let saqlandi2 = countries.shift()
+console.log(saqlandi2);
+
+let months = ['Jan', 'Feb', 'Mar', 'Apr'];
+months.splice(0, 2, 'May', 'Jun');
+console.log(months);
+
+
+let items = ['book', 'pen', 'pencil', 'eraser', 'ruler'];
+items.pop();
+items.pop();
+items.pop();
+console.log(items);
+
+let numbers2 = [1, 2, 3];
+numbers2.splice(3, 0, 1, 1);
+console.log(numbers2);
+
+let data = ['middle'];
+data.push('end');
+data.unshift('start');
+console.log(data);
+
+let digits = [0, 1, 2, 3, 4, 5];
+digits.splice(3, 1);
+console.log(digits);
+
+let oldArray = [10, 20, 30, 40];
+oldArray.push(50);
+oldArray.pop();
+console.log(oldArray)
+
+
+let words = ['hello', 'world', 'javascript'];
+words.shift();
+words.unshift('hi');
+console.log(words);
